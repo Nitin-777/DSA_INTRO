de4ee2c598a4196b090e2cd886f31d262372e602
Nitin <nitinsharmaop828@gmail.com>
2024-07-11 11:30:55 +0530

    insertion sort

---
+++ sorting c++/insertion.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void insertionsort(int arr[],int n){
+    for(int i=1; i<n; i++){
+        int temp=arr[i];
+        int j=i-1;
+        for(j; j>=0;j--){
+            if(arr[j]>temp){
+            arr[j+1]=arr[j];
+            }
+            else{
+                break;
+            }
+
+        }
+        arr[j+1]=temp;
+    }
+}
+int main()
+{
+    int arr[5]={4,8,1,2,3};
+    insertionsort(arr,5);
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
